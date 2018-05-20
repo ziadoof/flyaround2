ad2ace96fca8ac7943c44d1ca8c6dc46b6fe98ef
ziadoof <ziadoof@gmail.com>
2018-05-21 00:02:21 +0200

    routing

---
+++ src/AppBundle/Controller/ListingController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use AppBundle\Entity\Flight;
+use AppBundle\Entity\PlaneModel;
+use AppBundle\Entity\Reservation;
+
+    /**
+     * Listing controller.
+     *
+     * @Route("listing")
+     */
+
+class ListingController extends Controller
+{
+    /**
+     * List one reservation with one flight and one planemodel, with few IDs.
+     *
+     * @Route("/{reservation_id}/flight/{flight_id}/planemodel/{planemodel_id}", name="listing_index", requirements={"reservation_id": "\d+"})
+     * @Method("GET")
+     * @ParamConverter("reservation", options={"mapping": {"reservation_id": "id"}})
+     * @ParamConverter("flight", options={"mapping": {"flight_id": "id"}})
+     * @ParamConverter("planemodel", options={"mapping": {"planemodel_id": "id"}})
+     */
+
+    public function indexAction(Reservation $reservation, Flight $flight, PlaneModel $planemodel)
+    {
+        return $this->render('listing/index.html.twig', array(
+            'reservation' => $reservation,
+            'flight' => $flight,
+            'planemodel' => $planemodel
+        ));
+    }
+}
