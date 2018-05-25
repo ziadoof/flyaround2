28791fa2d0d0c5de4d091341ec5d312283691df9
ziadoof <ziadoof@gmail.com>
2018-05-25 13:13:35 +0200

    last

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+import $ from "jquery"
+
+$(document).ready(function () {
+    console.log("ok, JQuery fonctionne !");
+});
+console.log('Bienvenue sur WebPack!')
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+    var Encore = require('@symfony/webpack-encore');
+
+    Encore
+        .setOutputPath('web/build/')
+        .setPublicPath('/web')
+        .addEntry('app', './assets/js/app.js')
+        .addEntry('style', './assets/scss/main.scss')
+        .cleanupOutputBeforeBuild()
+        .enableBuildNotifications()
+        .enableSassLoader(function(sassOptions) {}, {
+            resolveUrlLoader: false
+             });
+
+    module.exports = Encore.getWebpackConfig();
